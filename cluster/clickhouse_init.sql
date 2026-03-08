f5f3f2bb0e69192047a0071cf3ff0206c5108dba
Muhammad Saad Akmal <120157311+saadakmal460@users.noreply.github.com>
2026-03-08 21:44:55 +0500

    Timeseries compatiability

--- cluster/clickhouse_init.sql
+++ cluster/clickhouse_init.sql
@@ -5,78 +5,30 @@ CREATE TABLE logging.logs
     projectId String,
     correlationId String DEFAULT '',
 
-    timestamp DateTime64(3),
+    timestamp DateTime64(3) DEFAULT now64(3),
 
     level LowCardinality(String),
-    message String,
     component LowCardinality(String),
 
-    meta String,
-    exception String,
+    message String CODEC(ZSTD),
+    meta String CODEC(ZSTD),
+    exception String CODEC(ZSTD),
 
     sessionStartedAt Nullable(DateTime64(3)),
     sessionEndedAt Nullable(DateTime64(3)),
 
     durationMs Nullable(UInt64),
 
-    requestMethod Nullable(String),
+    requestMethod LowCardinality(Nullable(String)),
     requestPath Nullable(String),
-    statusCode Nullable(UInt16)
+    statusCode Nullable(UInt16),
+
+    INDEX idx_level level TYPE set(100) GRANULARITY 4,
+    INDEX idx_status statusCode TYPE minmax GRANULARITY 4
 )
 ENGINE = MergeTree()
 PARTITION BY toYYYYMM(timestamp)
-ORDER BY (projectId, timestamp, correlationId)
+ORDER BY (projectId, timestamp)
+TTL timestamp + INTERVAL 30 DAY
 SETTINGS index_granularity = 8192;
-
-
-CREATE TABLE logging.logs_kafka
-(
-    projectId String,
-    correlationId String,
-
-    timestamp DateTime64(3),
-
-    level String,
-    message String,
-    component String,
-
-    meta String,
-    exception String,
-
-    sessionStartedAt Nullable(DateTime64(3)),
-    sessionEndedAt Nullable(DateTime64(3)),
-
-    durationMs Nullable(UInt64),
-
-    requestMethod Nullable(String),
-    requestPath Nullable(String),
-    statusCode Nullable(UInt16)
-)
-ENGINE = Kafka
-SETTINGS
-    kafka_broker_list = 'kafka1:9092,kafka2:9092,kafka3:9092',
-    kafka_topic_list = 'logs',
-    kafka_group_name = 'clickhouse_logs',
-    kafka_format = 'JSONEachRow',
-    kafka_num_consumers = 6;
-
-CREATE MATERIALIZED VIEW logging.logs_mv
-TO logging.logs
-AS
-SELECT
-    projectId,
-    correlationId,
-    timestamp,
-    level,
-    message,
-    component,
-    meta,
-    exception,
-    sessionStartedAt,
-    sessionEndedAt,
-    durationMs,
-    requestMethod,
-    requestPath,
-    statusCode
-FROM logging.logs_kafka;
 
