4d802f269664b04ad8483f39911404d15e23f067
Kazim68 <120156815+Kazim68@users.noreply.github.com>
2026-05-07 18:36:23 +0500

    somehtng

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,6 +3,12 @@ const liveLogsRoutes = require("./liveLogs.routes");
 const logsRoutes = require("./logs.routes");
 
 async function registerRoutes(app) {
+  app.get("/", async (request, reply) => {
+    return reply
+      .code(200)
+      .send({ status: "ok", message: "LogiScout streaming server is running" });
+  });
+
   app.register(ingestRoutes);
   app.register(liveLogsRoutes);
   app.register(logsRoutes);
